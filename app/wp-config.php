5e86abc3e79443be3cf07d84d6c89aa70730c749
111110100 <elomibao@gmail.com>
2021-11-19 21:27:36 +0800

    Update wp-config.php
    Added: define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] . '/');

--- app/wp-config.php
+++ app/wp-config.php
@@ -77,7 +77,8 @@ define( 'WP_DEBUG', false );
  * configure HTTPS
  */
 define('FORCE_SSL_ADMIN', true);
-define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
+define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 $_SERVER['HTTPS']       = 'on';
 $_SERVER['SERVER_PORT'] = 443;
 
